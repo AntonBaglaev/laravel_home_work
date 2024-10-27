803a7c56189d69040a9cd88fb673ec63f04f972e
AntonBaglaev <abaglaev@yandex.ru>
2024-10-27 20:40:24 +0300

    Работали с добавлением маршрутов на примере книг

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,8 @@ Route::get('/test', \App\Http\Controllers\TestController::class);
 Route::get('/test', [\App\Http\Controllers\SimpleController::class, 'test']);
 Route::get('/user', [\App\Http\Controllers\UserController::class, 'index']); // форма добавления
 Route::post('/user', [\App\Http\Controllers\UserController::class, 'store'])->name('store-user');
+
+Route::get('/books', [\App\Http\Controllers\EntityController::class, 'index'])->name('books');
+Route::post('/books', [\App\Http\Controllers\EntityController::class, 'store'])->name('save_book');
+Route::get('/remove_book/{id?}', [\App\Http\Controllers\EntityController::class, 'delete'])->
+where(['id' => '[A-Za-z0-9]+'])->name('remove_book');
