6a385ef64ac0f82338ccfc22941bf3dd88d5329c
AntonBaglaev <abaglaev@yandex.ru>
2024-10-30 23:36:15 +0300

    Добавили запись в Бд

---
+++ app/Console/Commands/TestDatabase.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Employee;
+use Illuminate\Console\Command;
+
+class TestDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'database:test_insert';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        for($i=0; $i<5; $i++) {
+            $employee = new Employee();
+            $employee->first_name = 'John';
+            $employee->save();
+        }
+        return 0;
+    }
+}
